a0112f3c13dd8e207d3bd776ece4e00e259fb4fc
Olivier Hécart <olivier.hecart@adlinktech.com>
2019-09-10 15:49:40 +0200

    Test evals

--- test/z_client_test.c
+++ test/z_client_test.c
@@ -22,8 +22,11 @@ z_mvar_t *z1_sto1_mvar = 0;
 resource z2_sto1_last_res;
 z_mvar_t *z2_sto1_mvar = 0;
 
-z_list_t *replies = 0;
-z_mvar_t *replies_mvar = 0;
+z_list_t *storage_replies = 0;
+z_mvar_t *storage_replies_mvar = 0;
+
+z_list_t *eval_replies = 0;
+z_mvar_t *eval_replies_mvar = 0;
 
 void z1_sub1_listener(const z_resource_id_t *rid, const unsigned char *data, size_t length, const z_data_info_t *info, void *arg) {    
   Z_UNUSED_ARG_3(length, info, arg);
@@ -89,6 +92,44 @@ void z2_sto1_handler(const char *rname, const char *predicate, replies_sender_t 
   send_replies(query_handle, replies);
 }
 
+void z1_eval1_handler(const char *rname, const char *predicate, replies_sender_t send_replies, void *query_handle, void *arg) {
+  Z_UNUSED_ARG_3(rname, predicate, arg);
+  z_resource_t resource;
+  resource.rname = "/test/client/z1_eval1";
+  uint8_t data = 33;
+  resource.data = &data;
+  resource.length = 1;
+  resource.encoding = 0;
+  resource.kind = 0;
+
+  z_resource_t *p_resource = &resource;
+
+  z_array_resource_t replies;
+  replies.length = 1;
+  replies.elem = &p_resource;
+
+  send_replies(query_handle, replies);
+}
+
+void z2_eval1_handler(const char *rname, const char *predicate, replies_sender_t send_replies, void *query_handle, void *arg) {
+  Z_UNUSED_ARG_3(rname, predicate, arg);
+  z_resource_t resource;
+  resource.rname = "/test/client/z2_eval1";
+  uint8_t data = 66;
+  resource.data = &data;
+  resource.length = 1;
+  resource.encoding = 0;
+  resource.kind = 0;
+
+  z_resource_t *p_resource = &resource;
+
+  z_array_resource_t replies;
+  replies.length = 1;
+  replies.elem = &p_resource;
+
+  send_replies(query_handle, replies);
+}
+
 void reply_handler(const z_reply_value_t *reply, void *arg) {
   Z_UNUSED_ARG(arg);
   resource *res;
@@ -97,12 +138,20 @@ void reply_handler(const z_reply_value_t *reply, void *arg) {
       res = (resource*)malloc(sizeof(resource));
       res->name = strdup(reply->rname);
       res->data = *reply->data;
-      replies = z_list_cons(replies, res);
+      storage_replies = z_list_cons(storage_replies, res);
+      break;
+    case Z_EVAL_DATA: 
+      res = (resource*)malloc(sizeof(resource));
+      res->name = strdup(reply->rname);
+      res->data = *reply->data;
+      eval_replies = z_list_cons(eval_replies, res);
       break;
     case Z_STORAGE_FINAL:
+    case Z_EVAL_FINAL:
       break;
     case Z_REPLY_FINAL:
-      z_mvar_put(replies_mvar, replies);
+      z_mvar_put(storage_replies_mvar, storage_replies);
+      z_mvar_put(eval_replies_mvar, eval_replies);
       break;
   }
 }
@@ -114,9 +163,13 @@ int main(int argc, char **argv) {
   z2_sub1_mvar = z_mvar_empty();
   z1_sto1_mvar = z_mvar_empty();
   z2_sto1_mvar = z_mvar_empty();
-  replies_mvar = z_mvar_empty();
+  storage_replies_mvar = z_mvar_empty();
+  eval_replies_mvar = z_mvar_empty();
 
   char *locator = strdup("tcp/127.0.0.1:7447");
+
+  z_query_dest_t best_match = {Z_BEST_MATCH, 0};
+  z_query_dest_t none = {Z_NONE, 0};
 
   z_zenoh_p_result_t z1_r = z_open(locator, 0, 0);
   ASSERT_RESULT(z1_r, "Unable to open session with broker")
@@ -136,6 +189,10 @@ int main(int argc, char **argv) {
   z_sto_p_result_t z1_sto1_r = z_declare_storage(z1, "/test/client/**", z1_sto1_listener, z1_sto1_handler, NULL);
   ASSERT_P_RESULT(z1_sto1_r, "Unable to declare storage\n");
   z_sto_t *z1_sto1 = z1_sto1_r.value.sto;
+
+  z_eval_p_result_t z1_eval1_r = z_declare_eval(z1, "/test/client/z1_eval1", z1_eval1_handler, NULL);
+  ASSERT_P_RESULT(z1_eval1_r, "Unable to declare eval\n");
+  z_eva_t *z1_eval1 = z1_eval1_r.value.eval;
 
   z_pub_p_result_t z1_pub1_r = z_declare_publisher(z1, "/test/client/z1_pub1");
   ASSERT_P_RESULT(z1_pub1_r, "Unable to declare publisher\n");
@@ -157,6 +214,10 @@ int main(int argc, char **argv) {
   z_sto_p_result_t z2_sto1_r = z_declare_storage(z1, "/test/client/**", z2_sto1_listener, z2_sto1_handler, NULL);
   ASSERT_P_RESULT(z2_sto1_r, "Unable to declare storage\n");
   z_sto_t *z2_sto1 = z2_sto1_r.value.sto;
+
+  z_eval_p_result_t z2_eval1_r = z_declare_eval(z2, "/test/client/z2_eval1", z2_eval1_handler, NULL);
+  ASSERT_P_RESULT(z2_eval1_r, "Unable to declare eval\n");
+  z_eva_t *z2_eval1 = z2_eval1_r.value.eval;
 
   z_pub_p_result_t z2_pub1_r = z_declare_publisher(z2, "/test/client/z2_pub1");
   ASSERT_P_RESULT(z2_pub1_r, "Unable to declare publisher\n");
@@ -187,20 +248,76 @@ int main(int argc, char **argv) {
   assert(sent_res.data == rcvd_res->data);
 
   z_query(z1, "/test/client/**", "", reply_handler, NULL);
-  z_mvar_get(replies_mvar);
-  assert(2 == z_list_len(replies));
-  rcvd_res = z_list_head(replies);
+  z_mvar_get(storage_replies_mvar);
+  assert(2 == z_list_len(storage_replies));
+  rcvd_res = z_list_head(storage_replies);
   assert(0 == strcmp(sent_res.name, rcvd_res->name));
   assert(sent_res.data == rcvd_res->data);
-  z_list_free(&replies);
+  z_list_free(&storage_replies);
+  z_mvar_get(eval_replies_mvar);
+  assert(2 == z_list_len(eval_replies));
+  z_list_free(&eval_replies);
+
+  z_query_wo(z1, "/test/client/**", "", reply_handler, NULL, best_match, none);
+  z_mvar_get(storage_replies_mvar);
+  assert(2 == z_list_len(storage_replies));
+  rcvd_res = z_list_head(storage_replies);
+  assert(0 == strcmp(sent_res.name, rcvd_res->name));
+  assert(sent_res.data == rcvd_res->data);
+  z_list_free(&storage_replies);
+  z_mvar_get(eval_replies_mvar);
+  // assert(0 == z_list_len(eval_replies));
+  // This may not be true for now as : 
+  //  - zenoh-c does not check received query properties
+  //  - zenohd does not filter out replies
+  z_list_free(&eval_replies);
+
+  z_query_wo(z1, "/test/client/**", "", reply_handler, NULL, none, best_match);
+  z_mvar_get(storage_replies_mvar);
+  // assert(0 == z_list_len(storage_replies));
+  // This may not be true for now as : 
+  //  - zenoh-c does not check received query properties
+  //  - zenohd does not filter out replies
+  z_list_free(&storage_replies);
+  z_mvar_get(eval_replies_mvar);
+  assert(2 == z_list_len(eval_replies));
+  z_list_free(&eval_replies);
 
   z_query(z2, "/test/client/**", "", reply_handler, NULL);
-  z_mvar_get(replies_mvar);
-  assert(2 == z_list_len(replies));
-  rcvd_res = z_list_head(replies);
+  z_mvar_get(storage_replies_mvar);
+  assert(2 == z_list_len(storage_replies));
+  rcvd_res = z_list_head(storage_replies);
   assert(0 == strcmp(sent_res.name, rcvd_res->name));
   assert(sent_res.data == rcvd_res->data);
-  z_list_free(&replies);
+  z_list_free(&storage_replies);
+  z_mvar_get(eval_replies_mvar);
+  assert(2 == z_list_len(eval_replies));
+  z_list_free(&eval_replies);
+
+  z_query_wo(z2, "/test/client/**", "", reply_handler, NULL, best_match, none);
+  z_mvar_get(storage_replies_mvar);
+  assert(2 == z_list_len(storage_replies));
+  rcvd_res = z_list_head(storage_replies);
+  assert(0 == strcmp(sent_res.name, rcvd_res->name));
+  assert(sent_res.data == rcvd_res->data);
+  z_list_free(&storage_replies);
+  z_mvar_get(eval_replies_mvar);
+  // assert(0 == z_list_len(eval_replies));
+  // This may not be true for now as : 
+  //  - zenoh-c does not check received query properties
+  //  - zenohd does not filter out replies
+  z_list_free(&eval_replies);
+
+  z_query_wo(z2, "/test/client/**", "", reply_handler, NULL, none, best_match);
+  z_mvar_get(storage_replies_mvar);
+  // assert(0 == z_list_len(storage_replies));
+  // This may not be true for now as : 
+  //  - zenoh-c does not check received query properties
+  //  - zenohd does not filter out replies
+  z_list_free(&storage_replies);
+  z_mvar_get(eval_replies_mvar);
+  assert(2 == z_list_len(eval_replies));
+  z_list_free(&eval_replies);
 
 
   sent_res.name = "/test/client/**";
@@ -220,20 +337,26 @@ int main(int argc, char **argv) {
   assert(sent_res.data == rcvd_res->data);
 
   z_query(z1, "/test/client/**", "", reply_handler, NULL);
-  z_mvar_get(replies_mvar);
-  assert(2 == z_list_len(replies));
-  rcvd_res = z_list_head(replies);
+  z_mvar_get(storage_replies_mvar);
+  assert(2 == z_list_len(storage_replies));
+  rcvd_res = z_list_head(storage_replies);
   assert(0 == strcmp(sent_res.name, rcvd_res->name));
   assert(sent_res.data == rcvd_res->data);
-  z_list_free(&replies);
+  z_list_free(&storage_replies);
+  z_mvar_get(eval_replies_mvar);
+  assert(2 == z_list_len(eval_replies));
+  z_list_free(&eval_replies);
 
   z_query(z2, "/test/client/**", "", reply_handler, NULL);
-  z_mvar_get(replies_mvar);
-  assert(2 == z_list_len(replies));
-  rcvd_res = z_list_head(replies);
+  z_mvar_get(storage_replies_mvar);
+  assert(2 == z_list_len(storage_replies));
+  rcvd_res = z_list_head(storage_replies);
   assert(0 == strcmp(sent_res.name, rcvd_res->name));
   assert(sent_res.data == rcvd_res->data);
-  z_list_free(&replies);
+  z_list_free(&storage_replies);
+  z_mvar_get(eval_replies_mvar);
+  assert(2 == z_list_len(eval_replies));
+  z_list_free(&eval_replies);
 
 
   sent_res.name = "/test/client/z1_pub1";
@@ -253,20 +376,26 @@ int main(int argc, char **argv) {
   assert(sent_res.data == rcvd_res->data);
 
   z_query(z1, "/test/client/**", "", reply_handler, NULL);
-  z_mvar_get(replies_mvar);
-  assert(2 == z_list_len(replies));
-  rcvd_res = z_list_head(replies);
+  z_mvar_get(storage_replies_mvar);
+  assert(2 == z_list_len(storage_replies));
+  rcvd_res = z_list_head(storage_replies);
   assert(0 == strcmp(sent_res.name, rcvd_res->name));
   assert(sent_res.data == rcvd_res->data);
-  z_list_free(&replies);
+  z_list_free(&storage_replies);
+  z_mvar_get(eval_replies_mvar);
+  assert(2 == z_list_len(eval_replies));
+  z_list_free(&eval_replies);
 
   z_query(z2, "/test/client/**", "", reply_handler, NULL);
-  z_mvar_get(replies_mvar);
-  assert(2 == z_list_len(replies));
-  rcvd_res = z_list_head(replies);
+  z_mvar_get(storage_replies_mvar);
+  assert(2 == z_list_len(storage_replies));
+  rcvd_res = z_list_head(storage_replies);
   assert(0 == strcmp(sent_res.name, rcvd_res->name));
   assert(sent_res.data == rcvd_res->data);
-  z_list_free(&replies);
+  z_list_free(&storage_replies);
+  z_mvar_get(eval_replies_mvar);
+  assert(2 == z_list_len(eval_replies));
+  z_list_free(&eval_replies);
 
 
   sent_res.name = "/test/client/z2_pub1";
@@ -286,25 +415,27 @@ int main(int argc, char **argv) {
   assert(sent_res.data == rcvd_res->data);
 
   z_query(z1, "/test/client/**", "", reply_handler, NULL);
-  z_mvar_get(replies_mvar);
-  assert(2 == z_list_len(replies));
-  rcvd_res = z_list_head(replies);
+  z_mvar_get(storage_replies_mvar);
+  assert(2 == z_list_len(storage_replies));
+  rcvd_res = z_list_head(storage_replies);
   assert(0 == strcmp(sent_res.name, rcvd_res->name));
   assert(sent_res.data == rcvd_res->data);
-  z_list_free(&replies);
+  z_list_free(&storage_replies);
 
   z_query(z2, "/test/client/**", "", reply_handler, NULL);
-  z_mvar_get(replies_mvar);
-  assert(2 == z_list_len(replies));
-  rcvd_res = z_list_head(replies);
+  z_mvar_get(storage_replies_mvar);
+  assert(2 == z_list_len(storage_replies));
+  rcvd_res = z_list_head(storage_replies);
   assert(0 == strcmp(sent_res.name, rcvd_res->name));
   assert(sent_res.data == rcvd_res->data);
-  z_list_free(&replies);
+  z_list_free(&storage_replies);
 
   z_undeclare_subscriber(z1_sub1);
   z_undeclare_subscriber(z2_sub1);
   z_undeclare_storage(z1_sto1);
   z_undeclare_storage(z2_sto1);
+  z_undeclare_eval(z1_eval1);
+  z_undeclare_eval(z2_eval1);
   z_undeclare_publisher(z1_pub1);
   z_undeclare_publisher(z2_pub1);
 
