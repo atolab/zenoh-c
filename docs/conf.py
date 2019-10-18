328fbb787371257c4e4dc36ce9bf6e92725a39ae
Olivier Hécart <olivier.hecart@adlinktech.com>
2019-10-18 16:25:23 +0200

    Initial doc

---
+++ docs/conf.py
@@ -0,0 +1,14 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information -----------------------------------------------------
+project = 'zenoh-c'
+copyright = '2019, ATOLabs'
+author = 'ATOLabs'
+release = '0.3.0'
+
+# -- General configuration ---------------------------------------------------
+extensions = []
+language = 'c'
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
